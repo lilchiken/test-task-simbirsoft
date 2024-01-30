5339ae9f701f93ea814d6a9016c2e0a35956ecb2
Ilia Andryukhin <andryuhin2@yandex.ru>
2024-01-30 15:06:19 +0300

    dev save

--- src/conf.py
+++ src/conf.py
@@ -12,20 +12,6 @@ __all__ = [
 ]
 
 
-USER_AGENT = (
-    """
-    Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0)
-    Gecko/20100101 Firefox/122.0
-    """
-)
-
-options = webdriver.FirefoxOptions()
-options.set_preference(
-    "general.useragent.override",
-    USER_AGENT
-)
-
-
 class DriverConf:
     command_executor = "http://localhost:4444"
     options = webdriver.FirefoxOptions()
